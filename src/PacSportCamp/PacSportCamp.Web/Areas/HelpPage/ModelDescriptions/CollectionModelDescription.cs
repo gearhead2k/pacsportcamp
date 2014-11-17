ea098197f526500862b9368ea57f0ff8343e02eb
James Pennington <gearhead25k@gmail.com>
2014-11-16 23:05:45 -0500

    Initial Solution Checkin

---
+++ src/PacSportCamp/PacSportCamp.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PacSportCamp.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ src/PacSportCamp/PacSportCamp.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PacSportCamp.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
